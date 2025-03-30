8ef76fb63c9969cb7eca5379cf01c79065683e8e
Ashutosh0624 <ashutosh.phd0602@gmail.com>
2025-03-30 02:37:38 +0000

    SOLID principle : Liskov subsitution principle

---
+++ SOLID_Principles/LSP.cpp
@@ -0,0 +1,49 @@
+/*
+ 
+LSP = Liskov substitution principle 
+
+What?
+   LSP says that derived class should behave like base class without surprising the user. 
+   LSP checks whether the behaviour is consistent or not across the base classes and derived classes. 
+
+   Hinglish explanation: base class behaviours jo expect kar raha , woh same derive class se bhi expect kiya jaata hai
+ 
+*/
+
+#include<iostream>
+using namespace std;
+
+class ISimulator{
+    public:
+    virtual void simulate() = 0;
+    virtual ~ISimulator();
+};
+
+class SpeedSimulator: public ISimulator{
+    public:
+    void simulate() override{
+        /*
+           This function should behave in the same way as base class's function is expected to behave.
+           This follows Liskov substituion principle.
+        */
+    }
+};
+
+class BrokenSimulator: public ISimulator{
+    public: 
+    void simulate() override{
+        throw std::runtime_error{
+            "Sensor failed!!"<<endl;  
+        }
+        /*
+            here , the derived class is not following LSP (Liskov substituition principle) as the behaviour is not able to simulate 
+            as per the base class simulate function is expected to do 
+        */
+    }
+}
+/*
+   
+  In both the cases, we can see that virtual function concept is applied but in the second case LSP is broken. 
+  SO, concept is LSP is implemented using virtual function  concept but condition is that base class and derived classes should behave similarly.
+
+*/
